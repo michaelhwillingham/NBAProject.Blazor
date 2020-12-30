cb0727e288014c76aea91d7a199eca825f64aff3
Michael Willingham <michaelhwillingham@gmail.com>
2020-12-30 12:58:58 -0600

    Add MediatR

--- NBAProject.Blazor/Startup.cs
+++ NBAProject.Blazor/Startup.cs
@@ -1,3 +1,4 @@
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -29,6 +30,7 @@ namespace NBAProject.Blazor
             services.AddSingleton<IMongoDbSettings>(serviceProvider =>
                 serviceProvider.GetRequiredService<IOptions<MongoDbSettings>>().Value);
             
+            services.AddMediatR(typeof(Startup));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
